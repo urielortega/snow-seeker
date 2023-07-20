9133c036c4486921807d7e207784130da192ea16
Uriel Ortega <urielortega2509@gmail.com>
2023-07-20 16:20:35 -0600

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Uriel Ortega on 20/07/23.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
